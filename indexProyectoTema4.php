75497ce92e0223fd948ec0272f33cafcdb10c297
SashaMGuerra <74042799+SashaMGuerra@users.noreply.github.com>
2021-11-03 10:12:43 +0100

    03/11/2021
    Añadido htaccess. Modificado nombre del índice.

--- indexProyectoTema4.php
+++ indexProyectoTema4.php
@@ -15,7 +15,20 @@
             <h2>Proyecto Tema 4</h2>
         </header>
         <main>
-            
+            <table>
+                <caption>Prácticas</caption>
+                <tr>
+                    <th rowspan="2">Enunciados</th>
+                    <th colspan="2">PDO</th>
+                    <th colspan="2">MySQLi</th>
+                </tr>
+                <tr>
+                    <th>Ejecutar</th>
+                    <th>Mostrar</th>
+                    <th>Ejecutar</th>
+                    <th>Mostrar</th>
+                </tr>
+            </table>
         </main>
         <footer>
             <div>Modificado el 02/1/2021 - Mª Isabel Martínez Guerra</div>
