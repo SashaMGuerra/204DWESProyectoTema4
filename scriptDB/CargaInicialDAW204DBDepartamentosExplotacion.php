edbea50885ab4d9e942880927c545078ac072015
SashaMGuerra <74042799+SashaMGuerra@users.noreply.github.com>
2021-11-19 13:59:13 +0100

    A 1&1 19/11/2021

--- scriptDB/CargaInicialDAW204DBDepartamentosExplotacion.php
+++ scriptDB/CargaInicialDAW204DBDepartamentosExplotacion.php
@@ -12,7 +12,7 @@ $sInstrucciones = <<<QUERY
     INSERT INTO Departamento VALUES
     ('VEN', 'Ventas', null, 250.51),
     ('COM', 'Compras', null, 200.30),
-    ('MAR', 'Marketing', null, 330.33
+    ('MAR', 'Marketing', null, 330.33)
 QUERY;
 
 try {
