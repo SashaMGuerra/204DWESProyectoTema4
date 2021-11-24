9e91b317f1c83b102421f930febbea75ef24f413
SashaMGuerra <74042799+SashaMGuerra@users.noreply.github.com>
2021-11-24 12:18:09 +0100

    Cambio menor 24/11/2021

--- codigoPHP/elementoFooter.php
+++ codigoPHP/elementoFooter.php
@@ -10,6 +10,6 @@
 
 <footer>
     <a href="https://github.com/SashaMGuerra/204DWESProyectoTema4"><img src="../webroot/media/img/github_logo_white.png" alt="repositorio"></a>
-    <div>© 2020-2021 Isabel Martínez Guerra — IES Los Sauces (Benavente, Zamora) — Modificado el 22/11/2021.</div>
+    <div>© 2021-2022 Isabel Martínez Guerra — IES Los Sauces (Benavente, Zamora) — Modificado el 22/11/2021.</div>
 </footer>
 
