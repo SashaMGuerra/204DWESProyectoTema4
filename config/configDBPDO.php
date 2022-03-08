37377010d6137b77ad3965ed922905c8a12531d2
Alejandra <74042799+SashaMGuerra@users.noreply.github.com>
2022-03-08 13:35:17 +0100

    Update configDBPDO.php

--- config/configDBPDO.php
+++ config/configDBPDO.php
@@ -7,23 +7,7 @@
  * Constantes para la conexión con la base de datos PDO.
  */
 
-// Desarrollo - sauces.
-define('HOST', 'mysql:host=daw204.sauces.local;dbname=DB204DWESProyectoTema4');
-define('USER', 'User204DWESProyectoTema4');
+
+define('HOST', 'mysql:host=direccionhost.local;dbname=DB204DWESAplicacionFinal');
+define('USER', 'usuario');
 define('PASSWORD', 'paso');
-
-// Desarrollo - Instituto.
-/*
-define('HOST', 'mysql:host=192.168.3.104;dbname=DAW204DBDepartamentos');
-define('USER', 'usuarioDAW204DBDepartamentos');
-define('PASSWORD', 'P@ssw0rd');
- * 
- */
-
-// Desarrollo - Casa.
-/*
-define('HOST', 'mysql:host=192.168.1.30;dbname=DAW204DBDepartamentos');
-define('USER', 'usuarioDAW204DBDepartamentos');
-define('PASSWORD', 'P@ssw0rd');
- * 
- */
