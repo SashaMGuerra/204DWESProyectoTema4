59b61e43ca88a8cbd80b5b6d9d08b3eb44c63ff9
Alejandra <74042799+SashaMGuerra@users.noreply.github.com>
2022-03-08 13:35:37 +0100

    Update configDBMySQLi.php

--- config/configDBMySQLi.php
+++ config/configDBMySQLi.php
@@ -10,25 +10,7 @@
  */
 
 // Desarrollo - sauces.
-define('HOST', 'daw204.sauces.local');
+define('HOST', 'direccionhost.local');
 define('USER', 'User204DWESProyectoTema4');
 define('PASSWORD', 'paso');
 define('DB', 'DB204DWESProyectoTema4');
-
-/*
-// Desarrollo - Instituto.
-define('HOST', '192.168.3.104');
-define('USER', 'usuarioDAW204DBDepartamentos');
-define('PASSWORD', 'P@ssw0rd');
-define('DB', 'Departamento');
- * 
- */
-
-// Desarrollo - Casa.
-/*
-define('HOST', '192.168.1.30');
-define('USER', 'usuarioDAW204DBDepartamentos');
-define('PASSWORD', 'P@ssw0rd');
-define('DB', 'DAW204DBDepartamentos');
- * 
- */
